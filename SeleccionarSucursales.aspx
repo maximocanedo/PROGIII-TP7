fe44064dbc314de75b6d97e6bab97ab34b8f77c0
Oli-Hanczyc <oli.hanczyc@gmail.com>
2023-05-29 19:07:49 -0300

    Primera parte del ejercicio 2 de SeleccionarSucursales.aspx

--- SeleccionarSucursales.aspx
+++ SeleccionarSucursales.aspx
@@ -177,7 +177,7 @@
                                 <br />
                                 <asp:Label ID="DescripcionSucursalLabel" runat="server" Text='<%# Eval("DescripcionSucursal") %>' />
                                 <br />
-                                <asp:Button ID="Button3" runat="server" Text="Seleccionar" />
+                                <asp:Button ID="btnSeleccionar" runat="server" Text="Seleccionar" CommandArgument='<%# Eval("NombreSucursal")+" "+Eval("DescripcionSucursal")+" "+Eval("URL_Imagen_Sucursal") %>' CommandName="eventoButton" OnCommand="btnSeleccionar_Command" />
                                 <br />
                             </td>
                         </ItemTemplate>
@@ -222,7 +222,7 @@
                     <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="Data Source=localhost\SQLEXPRESS;Initial Catalog=BDSucursales;Integrated Security=True" SelectCommand="SELECT [Id_Provincia], [DescripcionProvincia] FROM [Provincia]"></asp:SqlDataSource>
                 </td>
                 <td class="auto-style18">
-                    <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="Data Source=localhost\SQLEXPRESS;Initial Catalog=BDSucursales;Integrated Security=True" SelectCommand="SELECT [NombreSucursal], [DescripcionSucursal], [URL_Imagen_Sucursal] FROM [Sucursal]"></asp:SqlDataSource>
+                    <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:BDSucursalesConnectionString2 %>" SelectCommand="SELECT [NombreSucursal], [DescripcionSucursal], [URL_Imagen_Sucursal] FROM [Sucursal]"></asp:SqlDataSource>
                 </td>
             </tr>
         </table>
--- SeleccionarSucursales.aspx.cs
+++ SeleccionarSucursales.aspx.cs
@@ -4,17 +4,60 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using TrabajoPractico7.Clases;
 
 namespace TrabajoPractico7
 {
 	public partial class SeleccionarSucursales1 : System.Web.UI.Page
 	{
-		protected void Page_Load(object sender, EventArgs e)
+        protected const string SucursalesSeleccionadas = "SucursalesSeleccionadas";
+
+        protected void Page_Load(object sender, EventArgs e)
 		{
 
 		}
 
+        // PARA SELECCIONAR UN ITEM ESPECÍFICO:
+        protected void btnSeleccionar_Command(object sender, CommandEventArgs e)
+        {
+            if (e.CommandName == "eventoButton")
+            {
+                // Guardo los items seleccionados en Session.
+                if (Session[SucursalesSeleccionadas] == null)
+                {
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add(Sucursal.Columns.Nombre, typeof(string));
+                    dt.Columns.Add(Sucursal.Columns.Descripcion, typeof(string));
+                    dt.Columns.Add(Sucursal.Columns.Imagen, typeof(string));
+                    Session[SucursalesSeleccionadas] = dt;
+                }
+                Session[SucursalesSeleccionadas] = e.CommandArgument.ToString();
+               // DataTable dtSucursalesSel = (DataTable)Session[SucursalesSeleccionadas];
 
+                /* REFERENCIA - BORRAR LUEGO
+                    //VERIFICACION DE PRODUCTO REPETIDO
+                    bool productoExistente = dtProductosSel.AsEnumerable()
+                        .Any(row => row.Field<string>(Producto.Columns.Id) == s_IDProducto);
+
+                    //SI NO SE REPITE, SE AGREGA
+                    if (!productoExistente) {
+                        DataRow newRow = dtProductosSel.NewRow();
+                        newRow[Producto.Columns.Id] = s_IDProducto;
+                        newRow[Producto.Columns.Nombre] = s_NombreProducto;
+                        newRow[Producto.Columns.IdProveedor] = s_IDProveedor;
+                        newRow[Producto.Columns.PrecioUnitario] = s_PrecioUnitario;
+                        dtProductosSel.Rows.Add(newRow);
+
+                        //CARGAR TABLA ACTUALIZADA A LA VARIABLE SESSION
+                        Session[TablaProductosSeleccionados] = dtProductosSel;
+                        ShowSnackbar($"Seleccionado: {s_NombreProducto}");
+                    }
+                */
+            }
+        }
+
+        // PARA SELECCIONAR POR PROVINCIA:
         protected void eventoSeleccionar_Command(object sender, CommandEventArgs e)
         {
             if (e.CommandName == "eventoSeleccionar")
